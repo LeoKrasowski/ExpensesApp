aedc7b6d5e809f7d1336fbd22ee89a558654eb6d
LeoKrasowski <leokrasowski@gmail.com>
2024-05-12 22:06:58 +0200

    Initial commit

---
+++ deployment/native/styles.js
@@ -0,0 +1,11 @@
+import * as style0 from "C:/Users/A200119422/Mendix/ExpensesApp_Module6Completed_10_3_0/themesource/administration/native/main";
+import * as style1 from "C:/Users/A200119422/Mendix/ExpensesApp_Module6Completed_10_3_0/themesource/nanoflowcommons/native/main";
+import * as style2 from "C:/Users/A200119422/Mendix/ExpensesApp_Module6Completed_10_3_0/themesource/webactions/native/main";
+import * as style3 from "C:/Users/A200119422/Mendix/ExpensesApp_Module6Completed_10_3_0/themesource/atlas_core/native/main";
+import * as style4 from "C:/Users/A200119422/Mendix/ExpensesApp_Module6Completed_10_3_0/themesource/expenses/native/main";
+import * as style5 from "C:/Users/A200119422/Mendix/ExpensesApp_Module6Completed_10_3_0/themesource/notification/native/main";
+import * as style6 from "C:/Users/A200119422/Mendix/ExpensesApp_Module6Completed_10_3_0/theme/native/main";
+
+import { flatten } from "mendix/native";
+
+module.exports = flatten([style0, style1, style2, style3, style4, style5, style6]);
